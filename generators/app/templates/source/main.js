9bbe7eee3fc7ae1fedb6601c948202272b81208e
sajera <allsajera@gmail.com>
2016-11-22 14:04:14 +0200

    added options example for browser window

--- generators/app/templates/source/main.js
+++ generators/app/templates/source/main.js
@@ -4,16 +4,23 @@
 var url = require('url');
 var path = require('path');
 var electron = require('electron');
-var mainWindow = null;
+var win = null;
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 function createWindow () {
     // Create the browser window.
-    (mainWindow = new electron.BrowserWindow({
-        width: 800,
-        height: 600
+    (win = new electron.BrowserWindow({
+        width: 1000,
+        height: 800,
+        minWidth: 600,
+        minHeight: 400,
+        title: '<%=title%>',
+        autoHideMenuBar: true,
+        x: 100,
+        y: 20,
+        backgroundColor: '#2e2c29',
     }))
     // and load the index.html of the app.
     .loadURL( url.format({
@@ -22,19 +29,19 @@ function createWindow () {
         slashes: true
     }));
     // Open the DevTools.
-    mainWindow.webContents.openDevTools();
+    win.webContents.openDevTools();
     // reload window
-    // mainWindow.webContents.reloadIgnoringCache();
+    // win.webContents.reloadIgnoringCache();
 
-    mainWindow.on('closed', function () {
-        mainWindow = null;
+    win.on('closed', function () {
+        win = null;
     });
 };
 
 electron.app.on('ready', createWindow);
 
 electron.app.on('activate', function () {
-    mainWindow === null && createWindow();
+    win === null && createWindow();
 });
 
 electron.app.on('window-all-closed', function () {
